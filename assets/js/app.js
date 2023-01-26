5932b503e63ac2b5a0f2f0068d0a600a58605bf0
Steven Gates <gates.steven@gmail.com>
2023-01-26 13:36:45 -0600

    add sonogram

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,8 @@
     function init() {
         var router = new Router([
             new Route('home', 'assets/views/home.html', true),            
-            new Route('about', 'assets/views/about.html')
+            new Route('about', 'assets/views/about.html'),
+            new Route('sonogram', 'assets/views/sonogram.html')
         ]);
     }
     init();
