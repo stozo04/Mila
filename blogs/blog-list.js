84c6dcce3e7d59cebbab35f35844edb21a4b8096
Steven Gates <gates.steven@gmail.com>
2023-10-17 10:43:00 -0500

    blog working

--- blogs/blog-list.js
+++ blogs/blog-list.js
@@ -1,4 +1,5 @@
-(function ($, window, document, undefined) {
+// https://www.apptrino.com/assets/plugins/cube-portfolio/documentation/#options
+(function($, window, document, undefined) {
     'use strict';
 
     // init cubeportfolio
@@ -7,10 +8,10 @@
         search: '#js-search-blog-posts',
         layoutMode: 'grid',
         defaultFilter: '*',
-        animationType: '3dflip',
-        gapHorizontal: 70,
+        animationType: '3dflip', // Options: fadeOut, quicksand, boxShadow, bounceLeft, bounceTop, bounceBottom, moveLeft, slideLeft, fadeOutTop, sequentially, skew slideDelay 3d Flip, rotateSides flipOutDelay, flipOut unfold foldLeft, scaleDown scaleSides, frontRow flipBottom, rotateRoom
+        gapHorizontal: 35,
         gapVertical: 30,
-        gridAdjustment: 'responsive',
+        gridAdjustment: 'responsive', // Options: defualt, alignCenter, responsive
         mediaQueries: [{
             width: 1500,
             cols: 4,
@@ -28,18 +29,18 @@
                 gapHorizontal: 50,
                 gapVertical: 20,
             }
-        }, {
+        },
+        {
             width: 320,
             cols: 1,
             options: {
                 caption: '',
-                gapHorizontal: 50,
+                gapHorizontal: 50
             }
         }],
-        caption: 'revealBottom',
-        displayType: 'fadeIn',
-        displayTypeSpeed: 400,
-        singlePageDelegate: null,
+        caption: 'overlayBottomPush', // Options: revealBottom, pushTop, pushDown, revealTop, moveRight, moveLeft, overlayBottomPush, overlayBottom, overlayBottomReveal, overlayBottomAlong, overlayRightAlong, minimal, fadeIn, zoom
+        displayType: 'sequentially', // Options: default, fadeIn, lazyLoading, fadeInToTop, sequentially, bottomToTop, 
+        displayTypeSpeed: 80,
         plugins: {
             loadMore: {
                 element: '#js-loadMore-blogs',
@@ -50,77 +51,3 @@
     });
 })(jQuery, window, document);
 
-
-// (function($, window, document, undefined) {
-//     'use strict';
-
-//     // init cubeportfolio
-//     $('#js-grid-blog-posts').cubeportfolio({
-//         filters: '#js-filters-blog-posts',
-//         search: '#js-search-blog-posts',
-//         layoutMode: 'grid',
-//         defaultFilter: '*',
-//         animationType: 'quicksand',
-//         gapHorizontal: 35,
-//         gapVertical: 30,
-//         gridAdjustment: 'responsive',
-//         mediaQueries: [{
-//             width: 1500,
-//             cols: 5,
-//         }, {
-//             width: 1100,
-//             cols: 4,
-//         }, {
-//             width: 800,
-//             cols: 3,
-//         }, {
-//             width: 480,
-//             cols: 2,
-//             options: {
-//                 caption: '',
-//                 gapHorizontal: 30,
-//                 gapVertical: 10,
-//             }
-//         }],
-//         caption: 'overlayBottomPush',
-//         displayType: 'sequentially',
-//         displayTypeSpeed: 80,
-
-//         // lightbox
-//         lightboxDelegate: '.cbp-lightbox',
-//         lightboxGallery: true,
-//         lightboxTitleSrc: 'data-title',
-//         lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
-
-//         // singlePage popup
-//         singlePageDelegate: '.cbp-singlePage',
-//         singlePageDeeplinking: true,
-//         singlePageStickyNavigation: true,
-//         singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
-//         singlePageCallback: function(url, element) {
-//             // to update singlePage content use the following method: this.updateSinglePage(yourContent)
-//             var t = this;
-
-//             $.ajax({
-//                     url: url,
-//                     type: 'GET',
-//                     dataType: 'html',
-//                     timeout: 30000
-//                 })
-//                 .done(function(result) {
-//                     t.updateSinglePage(result);
-//                 })
-//                 .fail(function() {
-//                     t.updateSinglePage('AJAX Error! Please refresh the page!');
-//                 });
-//         },
-
-//         plugins: {
-//             loadMore: {
-//                 element: '#js-loadMore-blogs',
-//                 action: 'click',
-//                 loadItems: 3,
-//             }
-//         },
-//     });
-// })(jQuery, window, document);
